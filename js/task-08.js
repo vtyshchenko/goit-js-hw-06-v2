831038bbfde3f31b7f6f64b4d933bf2b3731bf12
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-10-26 17:16:06 +0300

    task 08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
+const loginButton = loginForm.querySelector("button");
 
+loginForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (
+    e.currentTarget.email.value === "" ||
+    e.currentTarget.password.value === ""
+  ) {
+    alert("Error!!! Empty fields!");
+    return;
+  }
+
+  const data = {};
+  [...e.currentTarget.elements].forEach((elem) => {
+    if (elem.nodeName === "INPUT") {
+      data[elem.attributes.name.value] = elem.value;
+    }
+  });
+
+  console.log(data);
+  e.currentTarget.reset();
+}
