1cd45fe338ef13b9727206ee9e77b0b3551eaa7b
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-10-26 17:32:25 +0300

    task 09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeButton = document.querySelector(".change-color");
+
+changeButton.addEventListener("click", obButtonClick);
+
+function obButtonClick(e) {
+  const text = e.target.parentElement.querySelector(".color");
+  const colorValue = getRandomHexColor();
+  text.innerHTML = colorValue;
+  document.body.style.backgroundColor = colorValue;
+}
